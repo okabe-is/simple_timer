eb3997e634dba13fe7e78d99d67670ce23a94fdb
masaki.okabe <masaki.okabe@test.com>
2020-06-14 14:43:39 +0900

    add 2 components, repos card & tweet.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Timer from './Components/Timer'
+import {TwitterTweetEmbed} from 'react-twitter-embed'
 
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-            <h1>Simple Timer</h1>
-            <Timer />
+            <div>
+                <h1>Simple Timer</h1>
+                <Timer/>
+            </div>
+            <div className="footer">
+                <a href="https://github.com/okabe-is/simple_timer">
+                    <img
+                        src="https://gh-card.dev/repos/okabe-is/simple_timer.svg"/>
+                </a>
+                {/*#TODO need fix layout..*/}
+                <TwitterTweetEmbed tweetId="1272037174108536833"/>
+
+            </div>
         </div>
     );
 }
